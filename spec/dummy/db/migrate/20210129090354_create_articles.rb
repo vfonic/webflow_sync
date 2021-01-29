d251b85839bc69b01ad6862014c3f95ca8f4bb51
Viktor <viktor.fonic@gmail.com>
2021-01-29 10:04:59 +0100

    Run `rails g model Article title description:text published_at:datetime:index` and `rails db:migrate` from spec/dummy

---
+++ spec/dummy/db/migrate/20210129090354_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.datetime :published_at
+
+      t.timestamps
+    end
+    add_index :articles, :published_at
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    title { "MyString" }
+    description { "MyText" }
+    published_at { "2021-01-29 10:03:54" }
+  end
+end
