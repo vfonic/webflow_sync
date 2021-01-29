f813d2dca6a56755700532aaa2af7a34de730180
Viktor <viktor.fonic@gmail.com>
2021-01-29 14:49:41 +0100

    Improve code documentation

--- app/models/concerns/webflow_sync/item_sync.rb
+++ app/models/concerns/webflow_sync/item_sync.rb
@@ -9,10 +9,33 @@ module WebflowSync
       include WebflowSync::Callbacks
 
       # override this method to get more granular, per site customization
+      # for example, you could have Site model in your codebase:
+      #
+      # class Site < ApplicationRecord
+      #   has_many :articles
+      # end
+      #
+      # class Article < ApplicationRecord
+      #   belongs_to :site
+      #
+      #   def webflow_site_id
+      #     self.site.webflow_site_id
+      #   end
+      # end
+      #
       def webflow_site_id
         WebflowSync.configuration.webflow_site_id
       end
 
+      # You can customize this to your liking:
+      # def as_webflow_json
+      #   {
+      #     title: self.title.capitalize,
+      #     slug: self.title.parameterize,
+      #     published_at: self.created_at,
+      #     image: self.image_url
+      #   }
+      # end
       def as_webflow_json
         self.as_json
       end
