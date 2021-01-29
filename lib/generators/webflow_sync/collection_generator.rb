4274e005d7170bf4a4283d8941919d2e075629d0
Viktor <viktor.fonic@gmail.com>
2021-01-29 13:43:29 +0100

    Add collection sync generator

---
+++ lib/generators/webflow_sync/collection_generator.rb
@@ -0,0 +1,31 @@
+require 'rails/generators'
+require 'rails/generators/active_record'
+
+module WebflowSync
+  module Generators
+    class CollectionGenerator < Rails::Generators::NamedBase
+      desc "Registers ActiveRecord model to sync to WebFlow collection"
+
+      source_root File.expand_path('templates', __dir__)
+
+      include Rails::Generators::Migration
+      def add_migration
+        migration_template "migration.rb.erb", "#{migration_path}/add_webflow_item_id_to_#{table_name}.rb", migration_version: migration_version
+      end
+
+      def self.next_migration_number(dirname)
+        ActiveRecord::Generators::Base.next_migration_number(dirname)
+      end
+
+      private
+
+        def migration_path
+          ActiveRecord::Migrator.migrations_paths.first
+        end
+
+        def migration_version
+          "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+        end
+    end
+  end
+end
