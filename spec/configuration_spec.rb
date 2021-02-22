9dd7a868febed0be000ff1bca4abdcf586df8057
Viktor <viktor.fonic@gmail.com>
2021-02-22 14:10:18 +0100

    Fix: Configuration: Allow setting `skip_webflow_sync` to `false`

---
+++ spec/configuration_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module WebflowSync
+  RSpec.describe Configuration do
+    context 'when skip_webflow_sync is false' do
+      it 'is false' do
+        WebflowSync.configure do |config|
+          config.skip_webflow_sync = false
+        end
+
+        expect(WebflowSync.configuration.skip_webflow_sync).to be(false)
+      end
+    end
+
+    context 'when skip_webflow_sync is true' do
+      it 'is true' do
+        WebflowSync.configure do |config|
+          config.skip_webflow_sync = true
+        end
+
+        expect(WebflowSync.configuration.skip_webflow_sync).to be(true)
+      end
+    end
+  end
+end
