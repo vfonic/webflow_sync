503f5defb396e73c8af3d3c1b1c70ba11229745e
Jovana Stankovic <stankovicmjovana@gmail.com>
2021-06-28 14:15:40 +0200

    Use make_request method to avoid Webflow error: 'Rate limit hit'

--- app/services/webflow_sync/api.rb
+++ app/services/webflow_sync/api.rb
@@ -10,15 +10,14 @@ module WebflowSync
 
     def get_item(collection_slug, webflow_item_id)
       collection = find_webflow_collection(collection_slug)
-      client.item(collection['_id'], webflow_item_id)
+
+      make_request(:item, collection['_id'], webflow_item_id)
     end
 
-    def create_item(record, collection_slug) # rubocop:disable Metrics/MethodLength
+    def create_item(record, collection_slug)
       collection = find_webflow_collection(collection_slug)
-      response = client.create_item(
-        collection['_id'],
-        record.as_webflow_json.reverse_merge(_archived: false, _draft: false), live: true
-      )
+      response = make_request(:create_item, collection['_id'],
+                              record.as_webflow_json.reverse_merge(_archived: false, _draft: false), live: true)
 
       # use update_column to skip callbacks to prevent WebflowSync::ItemSync to kick off
       if record.update_column(:webflow_item_id, response['_id']) # rubocop:disable Rails/SkipsModelValidations
@@ -32,17 +31,16 @@ module WebflowSync
 
     def update_item(record, collection_slug)
       collection = find_webflow_collection(collection_slug)
-      response = client.update_item(
-        { '_cid' => collection['_id'], '_id' => record.webflow_item_id },
-        record.as_webflow_json.reverse_merge(_archived: false, _draft: false), live: true
-      )
+      response = make_request(:update_item, { '_cid' => collection['_id'], '_id' => record.webflow_item_id },
+                              record.as_webflow_json.reverse_merge(_archived: false, _draft: false), live: true)
+
       puts "Updated #{record.inspect} in #{collection_slug}"
       response
     end
 
     def delete_item(collection_slug, webflow_item_id)
       collection = find_webflow_collection(collection_slug)
-      response = client.delete_item({ '_cid' => collection['_id'], '_id' => webflow_item_id })
+      response = make_request(:delete_item, { '_cid' => collection['_id'], '_id' => webflow_item_id })
       puts "Deleted #{webflow_item_id} from #{collection_slug}"
       response
     end
@@ -62,6 +60,20 @@ module WebflowSync
         raise "Cannot find collection #{collection_slug} for Webflow site #{site_id}" unless response
 
         response
+      end
+
+      def make_request(method_name, *args, retries: 0, **kwargs)
+        if kwargs.present?
+          client.public_send(method_name, *args, **kwargs)
+        else
+          client.public_send(method_name, *args)
+        end
+      rescue Webflow::Error => e
+        raise if retries >= 8 || e.message.strip != 'Rate limit hit'
+
+        puts "Sleeping #{2**retries} seconds"
+        sleep 2**retries
+        make_request(method_name, *args, retries: retries + 1, **kwargs)
       end
   end
 end
