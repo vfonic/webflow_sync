0249ca96e89d7e362389c88c4994db11b618c48a
Jovana Stankovic <stankovicmjovana@gmail.com>
2021-07-02 13:58:39 +0200

    Enable running only focused spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,4 +27,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
